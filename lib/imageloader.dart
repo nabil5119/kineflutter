6a0d483d23a4848ea8e89ecfb6358df6abcf91f7
nabil5119 <nabil.zaghloul51@gmail.com>
2021-01-21 12:10:17 +0100

    Commit

---
+++ lib/imageloader.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+void main() => runApp(MyApp());
+
+class FireStorageService extends ChangeNotifier {
+  FireStorageService();
+  static Future<dynamic> loadImage(BuildContext context, String image) async {
+    return await FirebaseStorage.instance.ref().child(image).getDownloadURL();
+  }
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase Storage ',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoadFirbaseStorageImage(),
+    );
+  }
+}
+
+final Color yellow = Color(0xfffbc31b);
+final Color orange = Color(0xfffb6900);
+
+String image;
+
+class LoadFirbaseStorageImage extends StatefulWidget {
+  @override
+  _LoadFirbaseStorageImageState createState() =>
+      _LoadFirbaseStorageImageState();
+}
+
+class _LoadFirbaseStorageImageState extends State<LoadFirbaseStorageImage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: 360,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50.0),
+                    bottomRight: Radius.circular(50.0)),
+                gradient: LinearGradient(
+                    colors: [orange, yellow],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight)),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 80),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Text(
+                      "Loading image from Firebase Storage",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontStyle: FontStyle.italic),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Expanded(
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        height: double.infinity,
+                        margin: const EdgeInsets.only(
+                            left: 30.0, right: 30.0, top: 10.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(30.0),
+                          child: FutureBuilder(
+                            future: _getImage(context, image),
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState ==
+                                  ConnectionState.done)
+                                return Container(
+                                  height:
+                                      MediaQuery.of(context).size.height / 1.25,
+                                  width:
+                                      MediaQuery.of(context).size.width / 1.25,
+                                  child: snapshot.data,
+                                );
+
+                              if (snapshot.connectionState ==
+                                  ConnectionState.waiting)
+                                return Container(
+                                    height: MediaQuery.of(context).size.height /
+                                        1.25,
+                                    width: MediaQuery.of(context).size.width /
+                                        1.25,
+                                    child: CircularProgressIndicator());
+
+                              return Container();
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                loadButton(context),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget loadButton(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding:
+                const EdgeInsets.symmetric(vertical: 5.0, horizontal: 16.0),
+            margin: const EdgeInsets.only(
+                top: 30, left: 20.0, right: 20.0, bottom: 20.0),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [yellow, orange],
+                ),
+                borderRadius: BorderRadius.circular(30.0)),
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  //here we can put the imagepath
+                  image = 'images/2021-01-19 06:30:13.798355.png';
+                });
+              },
+              child: Text(
+                "Load Image",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<Widget> _getImage(BuildContext context, String image) async {
+    Image m;
+    await FireStorageService.loadImage(context, image).then((downloadUrl) {
+      m = Image.network(
+        downloadUrl.toString(),
+        fit: BoxFit.scaleDown,
+      );
+    });
+
+    return m;
+  }
+}
